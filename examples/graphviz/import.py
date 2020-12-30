ec22a5852af0d33bafc95fe91143aa1cddd7a204
ealizadeh <me@ealizadeh.com>
2020-12-30 18:31:02 -0500

    Replace htmllib with html.parser. Replace urllib2 to requests. Run Black and isort.

--- examples/graphviz/import.py
+++ examples/graphviz/import.py
@@ -1,27 +1,26 @@
-#!/usr/bin/env python
-'''
+"""
 This example shows the interals of certain Python modules when they are being
 imported.
-'''
-from pycallgraph import PyCallGraph
-from pycallgraph import Config
+"""
+from pycallgraph import Config, PyCallGraph
 from pycallgraph.output import GraphvizOutput
 
 
 def main():
     import_list = (
-        'pickle',
-        'htmllib',
-        'urllib2',
+        "pickle",
+        "html.parser",
+        "requests",
+        # TODO: import-requests.png is not generated as the other two libraries.
     )
     graphviz = GraphvizOutput()
     config = Config(include_stdlib=True)
 
     for module in import_list:
-        graphviz.output_file = 'import-{}.png'.format(module)
+        graphviz.output_file = f"import-{module}.png"
         with PyCallGraph(output=graphviz, config=config):
             __import__(module)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
