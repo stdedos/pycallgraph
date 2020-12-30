7154b4e8390f00440650dfd46dd52dd05852bb4d
ealizadeh <me@ealizadeh.com>
2020-12-30 18:39:15 -0500

    Replace urllib2 with requests library. Run Black and isort.

--- examples/graphviz/large.py
+++ examples/graphviz/large.py
@@ -1,24 +1,24 @@
-#!/usr/bin/env python
-'''
+"""
 This example is trying to make a large graph. You'll need some internet access
 for this to work.
-'''
+"""
 
-from pycallgraph import PyCallGraph
-from pycallgraph import Config
+from pycallgraph import Config, PyCallGraph
 from pycallgraph.output import GraphvizOutput
 
 
 def main():
     graphviz = GraphvizOutput()
-    graphviz.output_file = 'large.png'
+    graphviz.output_file = "large.png"
     config = Config(include_stdlib=True)
 
     with PyCallGraph(output=graphviz, config=config):
-        from urllib2 import urlopen
         from xml.dom.minidom import parseString
-        parseString(urlopen('http://w3.org/').read())
+
+        import requests
+
+        parseString(requests.get("https://w3.org/").content)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
