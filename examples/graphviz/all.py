a5b06250488033d0c787e7b9d8464ef829b1336d
ealizadeh <me@ealizadeh.com>
2020-12-30 22:55:01 -0500

    Update path handling when running all examples

--- examples/graphviz/all.py
+++ examples/graphviz/all.py
@@ -6,10 +6,9 @@ import subprocess
 from pathlib import Path
 
 current_path = Path(__file__).parent  # Get the directory of the current file
-
-examples = list(current_path.glob("*.py"))
-examples = [ex for ex in examples if ex.stem != "all"]
+all_py_files = list(current_path.glob("*.py"))
+examples = [ex for ex in all_py_files if ex.stem != "all"]
 
 for example in examples:
     print(f"Running {example} ...")
-    subprocess.call(f"python {current_path.joinpath(example)}", shell=True)
+    subprocess.call(f"python {example}", shell=True)
