5395781fa0b637d954fea041b8a9ada2af7ecbe2
ealizadeh <me@ealizadeh.com>
2021-01-17 18:05:24 -0500

    Update metadata.

--- pycallgraph/metadata.py
+++ pycallgraph/metadata.py
@@ -1,12 +1,12 @@
 # A different file to pycallgraph.py because of circular import problem
 
-__version__ = "1.0.1"
+__version__ = "2.0.0"
 __copyright__ = "Copyright 2021 Essi Alizadeh"
 __license__ = "GPLv2"
 __author__ = "Essi Alizadeh"
-__email__ = "pycallgraph@gakman.com"
+__email__ = "pycallgraph@ealizadeh.com"
 __url__ = "http://pycallgraph.slowchop.com/"
 __credits__ = [
-    "Essi Alizadeh",
+    "Essi Alizadeh  | Copyright 2021",
     "Gerald Kaszuba | Copyright 2007-2013",
 ]
